ff6f2ce839221683ee20e0e4a978c645313376aa
James DeFrancesco <jdefrancesco2@gmail.com>
2016-08-22 12:18:34 -0400

    html/css updates

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+$(#menu_1).click(function(){
+  $(#Rules).removeClass('show').removeClass('hide').addClass('show');
+  $(#Policy).removeClass('show').addClass('hide');
+});
+$(#menu_2).click(function(){
+  $(#Rules).removeClass('show').addClass('hide');
+  $(#Policy).removeClass('hide').addClass('show');
+});
